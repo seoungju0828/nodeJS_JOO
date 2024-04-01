1c890380ad61bcd9a17ceff9559806023e9b1171
seoungju0828 <ministar2006@gmail.com>
2024-04-01 12:08:28 +0900

    url에서 정보 가져오기 추가

--- http/httpServer3.js
+++ http/httpServer3.js
@@ -5,6 +5,13 @@ const url = require('url');
 const server = http.createServer();
     
 server.on('request', async(req, res) => {
+
+    console.log("method : ", req.method);
+    console.log("url1", req.url);
+    console.log("url2", url.parse(req.url));
+    console.log("url2-1", url.parse(req.url).pathname);
+    console.log("url3", url.parse(req.url).query);
+
     try {
         const data = await fs.readFile("./httpServer2.html");
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
