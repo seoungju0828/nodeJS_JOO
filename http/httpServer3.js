79a60f7fcde541bd089cfe5d6450e7f1472390c6
seoungju0828 <ministar2006@gmail.com>
2024-04-05 00:36:44 +0900

    도메인 입력시 해당 주소에 맞는 파일로 이동

--- http/httpServer3.js
+++ http/httpServer3.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const path = require('path');
 const fs = require('fs').promises;
 const url = require('url');
 
@@ -6,21 +7,27 @@ const server = http.createServer();
     
 server.on('request', async(req, res) => {
 
-    console.log("method : ", req.method);
-    console.log("url1", req.url);
-    console.log("url2", url.parse(req.url));
-    console.log("url2-1", url.parse(req.url).pathname);
-    console.log("url3", url.parse(req.url).query);
+    // console.log("method : ", req.method);
+    // console.log("url1", req.url);
+    // console.log("url2", url.parse(req.url));
+    // console.log("url2-1", url.parse(req.url).pathname);
+    // console.log("url3", url.parse(req.url).query);
 
     try {
-        const data = await fs.readFile("./httpServer2.html");
-        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-        res.end(data);
+        let pathFileName = url.parse(req.url).pathname;
+
+        if (pathFileName != "/favicon.ico") {
+            pathFileName = pathFileName == "/" ? "/base" : pathFileName;
+            const data = await fs.readFile(`.${pathFileName}.html`);
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+            res.end(data);
+        }
     } catch(err) {
         console.error(err);
         res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8'});
         res.end("err : ", err.message);
     }
+
 });
 
 server.listen(8088, () => {
